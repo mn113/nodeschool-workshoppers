5932431ec07bfdbc5d37e1162edfe3d2f99bec9c
Martin Nicholson <recyclebing+github@gmail.com>
2017-04-04 12:22:09 +0100

    Long hex string test

--- regex-adventure/11_alteration.js
+++ regex-adventure/11_alteration.js
@@ -1,4 +1,9 @@
 module.exports = function (str) {
-	// returns whether the string starts with `cat`, `dog`, or `robot` followed by a number to the end of the string.
-	return /^(cat|dog|robot)[0-9]+$/.test(str);
+	// the string contains exactly 8 columns of hex codes formatted like: 0xFF with one or more characters of trailing whitespace after each hex code to separate the columns.
+	var hex = '0x[a-fA-F0-9]{2}';
+	var hexSpace = hex + '[\s\\n]+';
+	var hexOptionalSpace = hex + '[\s\\n]*$';
+	var re = new RegExp('^' + hexSpace.repeat(7) + hexOptionalSpace);
+	//return re.test(str);
+	return /^0x[a-fA-F0-9]{2}[\s\\n]+0x[a-fA-F0-9]{2}[\s\\n]+0x[a-fA-F0-9]{2}[\s\\n]+0x[a-fA-F0-9]{2}[\s\\n]+0x[a-fA-F0-9]{2}[\s\\n]+0x[a-fA-F0-9]{2}[\s\\n]+0x[a-fA-F0-9]{2}[\s\\n]+0x[a-fA-F0-9]{2}[\s\\n]*$/.test(str);
 };
