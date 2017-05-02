750cbaba468edf6d3764c82610650948bf35e311
Martin Nicholson <recyclebing+github@gmail.com>
2017-05-02 22:06:49 +0100

    Sorting with lodash

--- lololodash/01.js
+++ lololodash/01.js
@@ -1,9 +1,15 @@
 // include the Lo-Dash library
 var _ = require("lodash");
 
+// 01. Filter a list, Object or JSON by a parameterised condition:
 var filterWhere = function(list) {
 	return _.filter(list, {active: true});
 };
 
+// 02. Sort an array by a named key:
+var sortBy = function(list) {
+	return _.sortBy(list, 'quantity').reverse();
+};
+
 // export the worker function as a nodejs module
-module.exports = filterWhere;
+module.exports = sortBy;
